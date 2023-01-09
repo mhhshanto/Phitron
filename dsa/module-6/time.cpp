7a39d43eeb82dd17161b2a5f2bb8cc5e53717198
shanto <hhassibul@gmail.com>
2023-01-09 23:45:33 +0600

    dsa

--- dsa/module-6/time.cpp
+++ dsa/module-6/time.cpp
@@ -1,20 +1,25 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+
 using namespace std;
+
+/*
+Time complexity = O(1);
+Memory complexity = O(1);
+*/
 
 int main()
 {
-  int a,b,c;
-  cin>>a>>b>>c;
+    int a, b, c; // O(1)
+    cin >> a >> b >> c;
+    int maxi = max({a,b,c});
+    int mini = min({a,b,c});
+    int sum = a+b+c;
+    int mul = a*b*c;
 
-  int maxi = max({a,b,c});
-  int mini = min({a,b,c});
-  int sum = a+b+c;
-  int mul = a*b*c;
-  cout<<maxi<<"\n";
-  cout<<mini<<"\n";
-  cout<<sum<<"\n";
-  cout<<mul<<"\n";
+    cout << maxi << "\n";
+    cout << mini << "\n";
+    cout << sum << "\n";
+    cout << mul << "\n";
 
-  return 0; 
+    return 0;
 }
-   
