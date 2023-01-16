40074d0e5e7573637f48863b0f8355b602122cf5
shanto <hhassibul@gmail.com>
2023-01-16 23:49:00 +0600

    dsa

---
+++ Sorting_Algorithm/quick2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  vector<int>quick_sort(vector<int>a){
+    
+  }
+  
+  system("pause");
+  return 0;
+}
---
+++ Sorting_Algorithm/quicksort.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> quick_sort(vector<int>a)
+{
+    if(a.size()<=1)return a;
+    int pivot=a.size()-1;
+    vector<int>b,c;
+    for(int i=0; i<a.size(); i++)
+    {
+        if(i==pivot)continue;
+        else if(a[i]<=a[pivot])b.push_back(a[i]);
+        else c.push_back(a[i]);
+    }
+    vector<int>sorted_a,sorted_b,sorted_c;
+    sorted_b=quick_sort(b);
+    sorted_c=quick_sort(c);
+    for(int i=0; i<sorted_b.size(); i++)
+    {
+        sorted_a.push_back(sorted_b[i]);
+    }
+    sorted_a.push_back(a[pivot]);
+    for(int i=0; i<sorted_c.size(); i++)
+    {
+        sorted_a.push_back(sorted_c[i]);
+    }
+    return sorted_a;
+}
+int main()
+{
+    int n;
+    vector<int>a;
+    cin>>n;
+    for(int i=0; i<n; i++)
+    {
+        int x;
+        cin>>x;
+        a.push_back(x);
+    }
+    vector<int>sorted_a1=quick_sort(a);
+    for(int i=0; i<sorted_a1.size(); i++)
+    {
+        cout<<sorted_a1[i]<<" ";
+    }
+    return 0;
+}
+// 7  4 3 2 1 6 4 8
