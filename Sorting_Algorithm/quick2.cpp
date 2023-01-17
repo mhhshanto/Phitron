c61127160964f00aa542bcd33e1a6cbac23d3096
shanto <hhassibul@gmail.com>
2023-01-17 23:45:19 +0600

    dsa

--- Sorting_Algorithm/quick2.cpp
+++ Sorting_Algorithm/quick2.cpp
@@ -1,13 +1,52 @@
-#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
-
+vector<int> quick_sort(vector<int>a)
+{
+  if(a.size()<=1){
+    return a;
+  }
+  int pivot = a.size()-1;
+  vector<int>b,c;
+  for (int i = 0; i < a.size(); i++)
+  {
+    if(i==pivot){
+      continue;
+    }else if(a[i]<=a[pivot])
+    {
+      b.push_back(a[i]);
+    }else{
+      c.push_back(a[i]);
+    }
+  }
+  vector<int>sorted_a,sorted_b,sorted_c;
+  sorted_b = quick_sort(b);
+  sorted_c = quick_sort(c);
+  for (int i = 0; i < b.size(); i++)
+  {
+    sorted_a.push_back(sorted_b[i]);
+  }
+  sorted_a.push_back(a[pivot]);
+for (int i = 0; i < c.size(); i++)
+  {
+    sorted_a.push_back(sorted_c[i]);
+  }
+return sorted_a;
+}
 int main()
 {
-  vector<int>quick_sort(vector<int>a){
-
-  }
-  
-  system("pause");
-  return 0; 
-} 
- 
+    int n;
+    vector<int>a;
+    cin>>n;
+    for(int i=0; i<n; i++)
+    {
+        int x;
+        cin>>x;
+        a.push_back(x);
+    }
+    vector<int>sorted_a1=quick_sort(a);
+    for(int i=0; i<sorted_a1.size(); i++)
+    {
+        cout<<sorted_a1[i]<<" ";
+    }
+    return 0;
+}
---
+++ link-list/implimentation-1.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+class node{
+  public:
+        int data;
+        node * nxt;
+};
+
+class LinkedList{
+  public:
+  node * head;
+  LinkedList(){
+   head = NULL;
+  }
+node* CreateNewNode(int value){
+  node *newnode = new node;
+  newnode->data = value;
+  newnode->nxt = NULL;
+  return newnode;
+}
+  void InsertAtHead(int value){
+node *a = CreateNewNode(value);
+if(head == NULL){
+  head = a;
+}
+  }
+  void Traverse(){
+node* a = head;
+while (a!= NULL)
+{
+  cout<<a->data<<" ";
+  a = a->nxt;
+}
+cout<<"\n";
+
+  }
+  void SearchDistinctValue(int value){
+
+  }
+  void SearchAllValue(){
+
+  }
+};
+int main()
+{
+LinkedList l;
+l.InsertAtHead(10);
+l.InsertAtHead(30);
+l.InsertAtHead(20);
+l.InsertAtHead(30);
+l.Traverse();
+l.SearchDistinctValue(10);
+l.SearchDistinctValue(5);
+// l.SearchAllValue(30);
+  return 0;
+}
